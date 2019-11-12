0401a3f6ba805834d2f119562010add620512d30
pwen <wenpeiying@gmail.com>
2019-11-11 21:32:08 -0800

    create tables query

--- projects/data_modeling_with_postgres/sql_queries.py
+++ projects/data_modeling_with_postgres/sql_queries.py
@@ -1,26 +1,67 @@
 # DROP TABLES
 
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+songplay_table_drop = "DROP TABLE IF EXISTS songplays"
+user_table_drop = "DROP TABLE IF EXISTS users"
+song_table_drop = "DROP TABLE IF EXISTS songs"
+artist_table_drop = "DROP TABLE IF EXISTS artists"
+time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
 
 songplay_table_create = """
+CREATE TABLE songplay (
+    songplay_id int,
+    start_time timestamp,
+    user_id int,
+    song_id int,
+    artist_id int,
+    session_id int,
+    level text,
+    location text,
+    user_agent text
+);
 """
 
 user_table_create = """
+CREATE TABLE users (
+    user_id int,
+    first_name text,
+    last_name text,
+    gender text,
+    level text
+);
 """
 
 song_table_create = """
+CREATE TABLE songs (
+    song_id int,
+    title text,
+    artist_id int,
+    year int,
+    duration float
+);
 """
 
 artist_table_create = """
+CREATE TABLE artists (
+    artist_id int,
+    name text,
+    location text,
+    latitude float,
+    longitute float
+);
 """
 
 time_table_create = """
+CREATE TABLE time (
+    start_time timestamp,
+    hour int,
+    day int,
+    week int,
+    month int,
+    year int,
+    weekday int
+);
 """
 
 # INSERT RECORDS
