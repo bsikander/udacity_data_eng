31cb354742f2643ed1c8e7c33dfc73c908bb6fe8
pwen <wenpeiying@gmail.com>
2020-02-09 09:25:57 -0700

    wraggling data in spark

---
+++ notes/data_lake/spark_pd.py
@@ -0,0 +1,22 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import asc, desc, udf
+from pyspark.sql.functions import sum as Fsum
+from pyspark.sql.types import IntegerType, StringType
+
+import datetime
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+path = "hdfs://ec2-34-218-86-174.us-west-2.compute.amazonaws.com:9000/sparkify/sparkify_log_small.json"
+spark = SparkSession.builder.appName("wrangling v1").getOrCreate()
+user_log = spark.read.json(path)
+
+user_log.select("page").dropDuplicates().sort("page").show()
+
+user_log.select(["userId", "firstname", "page", "song"]).where(
+    user_log.userId == "1046"
+).collect()
+
+# convert time from epoch to datetime
